11776c853814f42b9363f03c099963b205227881
VanVDC <vanbvy@gmail.com>
2020-12-22 18:20:44 -0500

    fix edit user error by removing reducer cap USER

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
-//chekc if production
+//check if production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')))
   app.get('*', (req, res) =>
