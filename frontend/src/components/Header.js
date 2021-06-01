7acbe56f36f661a834219da243baa85a059454d5
VanVDC <vanbvy@gmail.com>
2021-06-01 19:35:43 -0400

    reorder the header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,41 +1,26 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { LinkContainer } from 'react-router-bootstrap'
-import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
-import SearchBox from './SearchBox'
-import { logout } from '../actions/userActions'
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import SearchBox from './SearchBox';
+import { logout } from '../actions/userActions';
 
 const Header = () => {
-  const dispatch = useDispatch()
-  const userLogin = useSelector((state) => state.userLogin)
-  const { userInfo } = userLogin
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
   const logoutHandler = () => {
-    dispatch(logout())
-  }
+    dispatch(logout());
+  };
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-        <Container>
-          <LinkContainer to='/'>
-            <Navbar.Brand>
-              <img
-                alt='logo'
-                src='/images/racing.png'
-                width='30px'
-                height='30px'
-                className='d-inline-block align-top'
-              />{' '}
-              EV-Wheels
-            </Navbar.Brand>
-          </LinkContainer>
+        <Container className='header-container'>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Route
-                render={({ history }) => <SearchBox history={history} />}
-              />
               {'      '}
               <LinkContainer to='/support'>
                 <Nav.Link> Support</Nav.Link>
@@ -77,10 +62,23 @@ const Header = () => {
               )}
             </Nav>
           </Navbar.Collapse>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img
+                alt='logo'
+                src='/images/racing.png'
+                width='30px'
+                height='30px'
+                className='d-inline-block align-top'
+              />{' '}
+              EV-Wheels
+            </Navbar.Brand>
+          </LinkContainer>
+          <Route render={({ history }) => <SearchBox history={history} />} />
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
