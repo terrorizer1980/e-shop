11eabfbda97023abe8a2784ba96f8c1dc941e58c
VanVDC <vanbvy@gmail.com>
2021-06-01 19:41:56 -0400

    change the header color

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -16,66 +16,72 @@ const Header = () => {
   };
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-        <Container className='header-container'>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ml-auto'>
-              {'      '}
-              <LinkContainer to='/support'>
-                <Nav.Link> Support</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to='/cart'>
+      <Navbar
+        className='header-container'
+        bg='light'
+        variant='light'
+        expand='lg'
+        collapseOnSelect
+      >
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ml-auto'>
+            {'      '}
+            <LinkContainer to='/support'>
+              <Nav.Link> Support</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/cart'>
+              <Nav.Link>
+                {' '}
+                <i className='fas fa-shopping-cart'></i> Cart
+              </Nav.Link>
+            </LinkContainer>
+            {userInfo ? (
+              <NavDropdown title={userInfo.name} id='username'>
+                <LinkContainer to='/profile'>
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <LinkContainer to='/login'>
                 <Nav.Link>
-                  {' '}
-                  <i className='fas fa-shopping-cart'></i> Cart
+                  <i className='fas fa-user'></i> Sign In
                 </Nav.Link>
               </LinkContainer>
-              {userInfo ? (
-                <NavDropdown title={userInfo.name} id='username'>
-                  <LinkContainer to='/profile'>
-                    <NavDropdown.Item>Profile</NavDropdown.Item>
-                  </LinkContainer>
-                  <NavDropdown.Item onClick={logoutHandler}>
-                    Logout
-                  </NavDropdown.Item>
-                </NavDropdown>
-              ) : (
-                <LinkContainer to='/login'>
-                  <Nav.Link>
-                    <i className='fas fa-user'></i> Sign In
-                  </Nav.Link>
+            )}
+            {userInfo && userInfo.isAdmin && (
+              <NavDropdown title='Admin' id='adminmenu'>
+                <LinkContainer to='/admin/userlist'>
+                  <NavDropdown.Item>Users</NavDropdown.Item>
                 </LinkContainer>
-              )}
-              {userInfo && userInfo.isAdmin && (
-                <NavDropdown title='Admin' id='adminmenu'>
-                  <LinkContainer to='/admin/userlist'>
-                    <NavDropdown.Item>Users</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/admin/productlist'>
-                    <NavDropdown.Item>Products</NavDropdown.Item>
-                  </LinkContainer>
-                  <LinkContainer to='/admin/orderlist'>
-                    <NavDropdown.Item>Orders</NavDropdown.Item>
-                  </LinkContainer>
-                </NavDropdown>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-          <LinkContainer to='/'>
-            <Navbar.Brand>
-              <img
-                alt='logo'
-                src='/images/racing.png'
-                width='30px'
-                height='30px'
-                className='d-inline-block align-top'
-              />{' '}
-              EV-Wheels
-            </Navbar.Brand>
-          </LinkContainer>
-          <Route render={({ history }) => <SearchBox history={history} />} />
-        </Container>
+                <LinkContainer to='/admin/productlist'>
+                  <NavDropdown.Item>Products</NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to='/admin/orderlist'>
+                  <NavDropdown.Item>Orders</NavDropdown.Item>
+                </LinkContainer>
+              </NavDropdown>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+        {/* <Container className='header-container'> */}
+        <LinkContainer to='/'>
+          <Navbar.Brand>
+            <img
+              alt='logo'
+              src='/images/racing.png'
+              width='30px'
+              height='30px'
+              className='d-inline-block align-top'
+            />{' '}
+            EV-Wheels
+          </Navbar.Brand>
+        </LinkContainer>
+        <Route render={({ history }) => <SearchBox history={history} />} />
+        {/* </Container> */}
       </Navbar>
     </header>
   );
