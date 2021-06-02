f9800c0d20de82a525fa5a70f129a7cc3af5a1f3
VanVDC <vanbvy@gmail.com>
2021-06-01 20:09:58 -0400

    remove the style

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -16,56 +16,29 @@ const Header = () => {
   };
   return (
     <header>
-      <Navbar bg='light' variant='light' expand='lg' collapseOnSelect>
-        {/* <nav
-          className=' justify-content-space-between container-fluid'
-          style={{ width: '100%' }}
-        > */}
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className=' justify-content-start'>
-            {'      '}
-            <LinkContainer to='/support'>
-              <Nav.Link> Support</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to='/cart'>
-              <Nav.Link>
-                {' '}
-                <i className='fas fa-shopping-cart'></i> Cart
-              </Nav.Link>
-            </LinkContainer>
-            {userInfo ? (
-              <NavDropdown title={userInfo.name} id='username'>
-                <LinkContainer to='/profile'>
-                  <NavDropdown.Item>Profile</NavDropdown.Item>
-                </LinkContainer>
-                <NavDropdown.Item onClick={logoutHandler}>
-                  Logout
-                </NavDropdown.Item>
-              </NavDropdown>
-            ) : (
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <i className='fas fa-user'></i> Sign In
-                </Nav.Link>
-              </LinkContainer>
-            )}
-            {userInfo && userInfo.isAdmin && (
-              <NavDropdown title='Admin' id='adminmenu'>
-                <LinkContainer to='/admin/userlist'>
-                  <NavDropdown.Item>Users</NavDropdown.Item>
-                </LinkContainer>
-                <LinkContainer to='/admin/productlist'>
-                  <NavDropdown.Item>Products</NavDropdown.Item>
-                </LinkContainer>
-                <LinkContainer to='/admin/orderlist'>
-                  <NavDropdown.Item>Orders</NavDropdown.Item>
-                </LinkContainer>
-              </NavDropdown>
-            )}
-          </Nav>
-        </Navbar.Collapse>
-        {/* <Container className='header-container'> */}
+      <Navbar
+        className=' justify-content-between'
+        bg='light'
+        variant='light'
+        expand='lg'
+        collapseOnSelect
+      >
+        <Nav className=' justify-content-start'>
+          {'      '}
+          <LinkContainer to='/about-us'>
+            <Nav.Link> Our Story</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to='/support'>
+            <Nav.Link> Support</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to='/cart'>
+            <Nav.Link>
+              {' '}
+              <i className='fas fa-shopping-cart'></i> Cart
+            </Nav.Link>
+          </LinkContainer>
+        </Nav>
+
         <LinkContainer to='/'>
           <Navbar.Brand>
             <img
@@ -78,9 +51,38 @@ const Header = () => {
             EV-Wheels
           </Navbar.Brand>
         </LinkContainer>
-        <Route render={({ history }) => <SearchBox history={history} />} />
-        {/* </Container> */}
-        {/* </nav> */}
+        <div className='header-container'>
+          <Route render={({ history }) => <SearchBox history={history} />} />
+          {userInfo ? (
+            <NavDropdown title={userInfo.name} id='username'>
+              <LinkContainer to='/profile'>
+                <NavDropdown.Item>Profile</NavDropdown.Item>
+              </LinkContainer>
+              <NavDropdown.Item onClick={logoutHandler}>
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
+          ) : (
+            <LinkContainer to='/login'>
+              <Nav.Link>
+                <i className='fas fa-user'></i> Sign In
+              </Nav.Link>
+            </LinkContainer>
+          )}
+          {userInfo && userInfo.isAdmin && (
+            <NavDropdown title='Admin' id='adminmenu'>
+              <LinkContainer to='/admin/userlist'>
+                <NavDropdown.Item>Users</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to='/admin/productlist'>
+                <NavDropdown.Item>Products</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to='/admin/orderlist'>
+                <NavDropdown.Item>Orders</NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>
+          )}
+        </div>
       </Navbar>
     </header>
   );
