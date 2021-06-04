a44719a760d0c0b31098cbd51235abbbf58f6cab
VanVDC <vanbvy@gmail.com>
2021-06-04 19:52:09 -0400

    change the size

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { Container } from 'react-bootstrap';
 
-import Header from './components/Header';
+import Header from './components/header/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
