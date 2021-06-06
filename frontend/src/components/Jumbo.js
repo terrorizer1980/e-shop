6b89aea3ed873a33e9bfd12ae10a9bb53a42a0df
VanVDC <vanbvy@gmail.com>
2021-06-06 12:40:45 -0400

    removed boostsrap

--- frontend/src/components/Jumbo.js
+++ frontend/src/components/Jumbo.js
@@ -2,22 +2,18 @@ import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Header from './Header';
 const Jumbo = () => {
-  let styles = {
-    color: 'white',
-  };
-
   return (
     <>
       <Jumbotron className='jumbo-bg d-flex justify-content-center align-items-center '>
-        <div style={styles} className='text-center'>
+        <div className='jumbo-text'>
           <h1 className='jumbo-h1 text-capitalize font-weight-bold '>
             Welcome to EV-Wheels
           </h1>
           <h4 className='jumbo-h4 '>
             Find the best personal transport devices on the Web!
           </h4>
-          <span className='line'></span>
-          <a href='#' className='enter-btn'>
+          <div className='line'></div>
+          <a href='#' className='enter-btn btn-i--white'>
             ENTER The SHOP
           </a>
         </div>
