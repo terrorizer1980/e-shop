f9f9c9e757f292c82af08a013ac1b8e853ae2f3f
VanVDC <vanbvy@gmail.com>
2021-06-07 17:19:21 -0400

    add my video to the front

--- frontend/src/components/Jumbo.js
+++ frontend/src/components/Jumbo.js
@@ -2,10 +2,25 @@ import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import { LinkContainer } from 'react-router-bootstrap';
 
+const videoSource =
+  'https://ia601401.us.archive.org/12/items/bgvideo_202106/bgvideo.mp4';
+
 const Jumbo = () => {
   return (
     <>
       <Jumbotron className='jumbo-bg d-flex justify-content-center align-items-center '>
+        <video
+          className='myVideo '
+          playsinline
+          autoPlay='autoplay'
+          loop='loop'
+          fullscreen
+          muted
+          id='my-video-id'
+        >
+          <source src={videoSource} type='video/mp4' />
+        </video>
+
         <div className='jumbo-text'>
           <h1 className='jumbo-h1 text-capitalize font-weight-bold '>
             Welcome to EV-Wheels
