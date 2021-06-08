7949f722ae682b19d509c5023d8b2335f0ccfd56
VanVDC <vanbvy@gmail.com>
2021-06-08 18:00:48 -0400

    remove products from homepage and add to store screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,35 +1,35 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { Row, Col, Container } from 'react-bootstrap';
-import Message from '../components/Message';
-import Loader from '../components/Loader';
-import Product from '../components/Product';
-import Paginate from '../components/Paginate';
-import { listProducts } from '../actions/productActions';
-import ProductCarousel from '../components/ProductCarousel';
+// import { Link } from 'react-router-dom';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { Row, Col, Container } from 'react-bootstrap';
+// import Message from '../components/Message';
+// import Loader from '../components/Loader';
+// import Product from '../components/Product';
+// import Paginate from '../components/Paginate';
+// import { listProducts } from '../actions/productActions';
+// import ProductCarousel from '../components/ProductCarousel';
 import Jumbotron from '../components/Jumbo';
 import Meta from '../components/Meta';
 
 const HomeScreen = ({ match }) => {
-  const keyword = match.params.keyword;
+  // const keyword = match.params.keyword;
 
-  const pageNumber = match.params.pageNumber || 1;
+  // const pageNumber = match.params.pageNumber || 1;
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const productList = useSelector((state) => state.productList);
-  const { loading, error, products, pages, page } = productList;
+  // const productList = useSelector((state) => state.productList);
+  // const { loading, error, products, pages, page } = productList;
 
-  useEffect(() => {
-    dispatch(listProducts(keyword, pageNumber));
-  }, [dispatch, keyword, pageNumber]);
+  // useEffect(() => {
+  //   dispatch(listProducts(keyword, pageNumber));
+  // }, [dispatch, keyword, pageNumber]);
 
   return (
     <>
       <Meta />
       <Jumbotron />
-      <Container>
+      {/* <Container>
         {!keyword ? (
           <ProductCarousel />
         ) : (
@@ -58,7 +58,7 @@ const HomeScreen = ({ match }) => {
             />
           </>
         )}
-      </Container>
+      </Container> */}
     </>
   );
 };
