949b629938b6262f3ae518e47bf23083e24de774
VanVDC <vanbvy@gmail.com>
2021-06-14 20:55:10 -0400

    removed propTypes

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,4 @@
-import React from 'react'
-// import PropTypes from 'prop-types'
+import React from 'react';
 
 const Rating = ({ value, text, color }) => {
   return (
@@ -66,17 +65,11 @@ const Rating = ({ value, text, color }) => {
       </span>
       <span>{text && text}</span>
     </div>
-  )
-}
+  );
+};
 
 Rating.defaultProps = {
   color: '#f8e825',
-}
+};
 
-// Rating.propTypes = {
-//   value: PropTypes.number.isRequired,
-//   text: PropTypes.string.isRequired,
-//   color: PropTypes.string,
-// }
-
-export default Rating
+export default Rating;
