00e9fd1bf5934f10763c2c45611d1113248320df
VanVDC <vanbvy@gmail.com>
2021-06-17 20:27:46 -0400

    added social icons

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -9,7 +9,21 @@ const Footer = () => {
         <div className='footer-contact'>Copyright &copy; 2020 EV-Wheels</div>
 
         <div className='footer-social'>
-          <span>facebook</span>
+          <span>
+            <a href='https://www.facebook.com/'>
+              <i class='fab fa-facebook-f'></i>
+            </a>
+          </span>{' '}
+          <span>
+            <a href='https://twitter.com/'>
+              <i class='fab fa-twitter-square'></i>
+            </a>
+          </span>{' '}
+          <span>
+            <a href='https://www.instagram.com/'>
+              <i class='fab fa-instagram'></i>
+            </a>
+          </span>
         </div>
         <br />
         <div className='footer-copyright'>Copyright &copy; 2020 EV-Wheels</div>
