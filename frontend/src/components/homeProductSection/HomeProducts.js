90466db76ffb56be762b52bb6052698f69e16e2b
VanVDC <vanbvy@gmail.com>
2021-06-18 18:23:23 -0400

    added add cart handler

--- frontend/src/components/homeProductSection/HomeProducts.js
+++ frontend/src/components/homeProductSection/HomeProducts.js
@@ -3,11 +3,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import Loader from '../Loader';
 import './homeProduct.css';
 
-const HomeProducts = () => {
+const HomeProducts = ({ match }) => {
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
+  useEffect(() => {
+    dispatch(listProductDetails(match.params.id));
+  }, [dispatch, match]);
 
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`);
+  };
   return <div></div>;
 };
 
