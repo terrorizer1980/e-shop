f29e9dedcd3bbf8e5bb4ed2846fd4fa29fe1deac
VanVDC <vanbvy@gmail.com>
2021-06-22 20:05:47 -0400

    added top 4 products

--- frontend/src/components/homeProductSection/HomeProducts.js
+++ frontend/src/components/homeProductSection/HomeProducts.js
@@ -1,13 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Loader from '../Loader';
+import { listTopProducts } from '../../actions/productActions';
+
+import './homeProduct.css';
 
 const HomeProducts = () => {
+  const dispatch = useDispatch();
+  const productTopRated = useSelector((state) => state.productTopRated);
+  const { loading, error, products } = productTopRated;
+
+  useEffect(() => {
+    dispatch(listTopProducts());
+  }, [dispatch]);
+
   return (
     <div className='product-container'>
-      <div className='product-section'>
-        <img src='' alt='' />
-        <h2 className='product-name'>{}</h2>
-        <h3 className='product-price'>{}</h3>
-      </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        products
+          .filter((product, index) => index <= 3)
+          .map((product) => (
+            <div className='product-section' key={product._id}>
+              <img src={product.image} alt={product.name} />
+              <h2 className='product-name'>{product.name}</h2>
+              <h3 className='product-price'>{product.price}</h3>
+            </div>
+          ))
+      )}
     </div>
   );
 };
