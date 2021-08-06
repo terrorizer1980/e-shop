688be748af745f8c6236f7cbc6bb912b15fb7452
VanVDC <vanbvy@gmail.com>
2021-08-06 16:22:49 -0400

    added form compoennt to support component

--- frontend/src/components/form/Form.js
+++ frontend/src/components/form/Form.js
@@ -4,47 +4,37 @@ import React from 'react';
 const Form = () => {
   return (
     <div className='background'>
-      <div className='container'>
-        <div className='screen'>
-          <div className='screen-header'>
-            <div className='screen-header-left'>
-              <div className='screen-header-button close' />
-              <div className='screen-header-button maximize' />
-              <div className='screen-header-button minimize' />
-            </div>
-            <div className='screen-header-right'>
-              <div className='screen-header-ellipsis' />
-              <div className='screen-header-ellipsis' />
-              <div className='screen-header-ellipsis' />
-            </div>
+      <div className='form-container'>
+        <div className='form-screen'>
+          <div className='form-screen-header'>
+            <p className='form-title'>CONTACT</p>
           </div>
-          <div className='screen-body'>
-            <div className='screen-body-item left'>
+          <div className='form-screen-body'>
+            <div className='form-screen-body-item left'>
               <div className='app-title'>
-                <span>CONTACT</span>
-                <span>US</span>
-              </div>
-              <div className='app-contact'>
-                CONTACT INFO : +62 81 314 928 595
+                <span>TALK TO US</span>
+                <div className='app-contact'>
+                  ADDRESS : 5555 Wrong Lane, Holli, CA 30081
+                </div>
+
+                <div className='app-contact'>PHONE : 555 555 5555</div>
+                <div className='app-contact'>EMAIL : mike@evwheels.com</div>
               </div>
             </div>
-            <div className='screen-body-item'>
+            <div className='form-screen-body-item'>
               <div className='app-form'>
                 <div className='app-form-group'>
                   <input
                     className='app-form-control'
                     placeholder='NAME'
-                    defaultValue='Krisantus Wanandi'
+                    defaultValue='Mike Smith'
                   />
                 </div>
                 <div className='app-form-group'>
                   <input className='app-form-control' placeholder='EMAIL' />
                 </div>
                 <div className='app-form-group'>
-                  <input
-                    className='app-form-control'
-                    placeholder='CONTACT NO'
-                  />
+                  <input className='app-form-control' placeholder='NAME ' />
                 </div>
                 <div className='app-form-group message'>
                   <input className='app-form-control' placeholder='MESSAGE' />
@@ -56,33 +46,6 @@ const Form = () => {
               </div>
             </div>
           </div>
-        </div>
-        <div className='credits'>
-          inspired by
-          <a
-            className='credits-link'
-            href='https://dribbble.com/shots/2666271-Contact'
-            target='_blank'
-          >
-            <svg className='dribbble' viewBox='0 0 200 200'>
-              <g stroke='#ffffff' fill='none'>
-                <circle cx={100} cy={100} r={90} strokeWidth={20} />
-                <path
-                  d='M62.737004,13.7923523 C105.08055,51.0454853 135.018754,126.906957 141.768278,182.963345'
-                  strokeWidth={20}
-                />
-                <path
-                  d='M10.3787186,87.7261455 C41.7092324,90.9577894 125.850356,86.5317271 163.474536,38.7920951'
-                  strokeWidth={20}
-                />
-                <path
-                  d='M41.3611549,163.928627 C62.9207607,117.659048 137.020642,86.7137169 189.041451,107.858103'
-                  strokeWidth={20}
-                />
-              </g>
-            </svg>
-            Gururaj
-          </a>
         </div>
       </div>
     </div>
