a531592181f201f50183b6c7a61c303e437f726e
VanVDC <vanbvy@gmail.com>
2021-08-06 19:54:40 -0400

    added spec component

--- frontend/src/components/porductDetails/Specs.js
+++ frontend/src/components/porductDetails/Specs.js
@@ -1,32 +1,30 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
-
+import './specs.css';
 const Specs = ({ weight, speed, range, motorPower, battery }) => {
   return (
-    <div>
-      <Row className='my-3 textAlign-center'>
-        <Col md={2}></Col>
-        <Col md={2}>
+    <div className='spec-container'>
+      <div className='spec-list'>
+        <span md={2}>
           <h4>Weight</h4>
           {weight} Ibs
-        </Col>
-        <Col md={2}>
+        </span>
+        <span md={2}>
           <h4>Speed</h4>
           {speed} MPH
-        </Col>
-        <Col md={2}>
+        </span>
+        <span md={2}>
           <h4>Range</h4>
           {range} miles
-        </Col>
-        <Col md={2}>
+        </span>
+        <span md={2}>
           <h4>Motor</h4>
           {motorPower} W
-        </Col>
-        <Col md={2}>
+        </span>
+        <span md={2}>
           <h4>Battery</h4>
           {battery} Wh
-        </Col>
-      </Row>
+        </span>
+      </div>
     </div>
   );
 };
