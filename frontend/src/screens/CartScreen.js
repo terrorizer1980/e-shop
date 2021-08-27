062ad77cdbd2e6a9f67063b9aecc203b6300de52
VanVDC <vanbvy@gmail.com>
2021-08-27 10:43:34 -0400

    new look

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,38 +1,46 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
-import Meta from '../components/Meta'
-import Message from '../components/Message'
-import { addToCart, removeFromCart } from '../actions/cartActions'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import Meta from '../components/Meta';
+import Message from '../components/Message';
+import { addToCart, removeFromCart } from '../actions/cartActions';
 
 const CartScreen = ({ match, location, history }) => {
-  const productId = match.params.id
-  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+  const productId = match.params.id;
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const cart = useSelector((state) => state.cart)
-  const { cartItems } = cart
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   useEffect(() => {
     if (productId) {
-      dispatch(addToCart(productId, qty))
+      dispatch(addToCart(productId, qty));
     }
-  }, [dispatch, productId, qty])
+  }, [dispatch, productId, qty]);
 
   const removeFromCartHandler = (id) => {
-    dispatch(removeFromCart(id))
-  }
+    dispatch(removeFromCart(id));
+  };
   const checkoutHandler = () => {
-    history.push('/login?redirect=shipping')
-  }
+    history.push('/login?redirect=shipping');
+  };
 
   return (
     <>
       <Meta title={'Cart'} />
       <Row>
         <Col md={8}>
-          <h1>Shopping Cart</h1>
+          <h1 className='text-dark'>Shopping Cart</h1>
           {cartItems.length === 0 ? (
             <Message>
               Your cart is empty<Link to='/'> Go Back</Link>
@@ -112,7 +120,7 @@ const CartScreen = ({ match, location, history }) => {
         </Col>
       </Row>
     </>
-  )
-}
+  );
+};
 
-export default CartScreen
+export default CartScreen;
